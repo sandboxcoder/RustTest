1fb79a6b7d33dd4652676be0afcd3a661cafb839
Richard Osborne <rosborne255@yahoo.com>
2025-07-16 09:07:29 -0500

    add Vector struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod vector;
+
+use vector::Vector;
+
 fn main() {
-    println!("Rust starter project");
+    let origin: Vector = Vector { x: 5.2, y: 0.4, z: 6.0 };
+    println!("origin ({} {} {})", origin.x, origin.y, origin.z);
 }
---
+++ src/vector.rs
@@ -0,0 +1,6 @@
+
+pub struct Vector {
+    pub x: f32, 
+    pub y: f32,
+    pub z: f32
+}
