78d8b05e82b6bc7a36ce4c076668d605b5b25732
Richard Osborne <rosborne255@yahoo.com>
2025-07-21 22:02:41 -0500

    rename Vector -> Vector3

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 mod vector;
 
-use vector::Vector;
+use vector::Vector3;
 
 fn main() {
-    let a: Vector = Vector { x: 1.0, y: 0.0, z: 0.0 };
-    let b: Vector = Vector { x: 0.0, y: 1.0, z: 0.0 };
+    let a: Vector3 = Vector3 { x: 1.0, y: 0.0, z: 0.0 };
+    let b: Vector3 = Vector3 { x: 0.0, y: 1.0, z: 0.0 };
     println!("dot prod a*b ({})", a.dot(b));
 }
--- src/vector.rs
+++ src/vector.rs
@@ -1,11 +1,11 @@
 
-pub struct Vector {
+pub struct Vector3 {
     pub x: f32, 
     pub y: f32,
     pub z: f32
 }
-impl Vector {
-    pub fn dot(&self, other : Vector) -> f32 {
+impl Vector3 {
+    pub fn dot(&self, other : Vector3) -> f32 {
         return self.x * other.x + self.y * other.y + self.z + other.z;
     }
 }
