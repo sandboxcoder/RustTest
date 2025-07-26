bde200b0cfbe92b10140026f0f958f07ac67de0c
Richard Osborne <rosborne255@yahoo.com>
2025-07-26 13:14:54 -0500

    vector add function

--- src/vector3.rs
+++ src/vector3.rs
@@ -1,4 +1,4 @@
-
+use std::ops;
 pub struct Vector3 {
     pub x: f32, 
     pub y: f32,
@@ -6,6 +6,17 @@ pub struct Vector3 {
 }
 impl Vector3 {
     pub fn dot(&self, other : Vector3) -> f32 {
-        return self.x * other.x + self.y * other.y + self.z + other.z;
+        return self.x * other.x + self.y * other.y + self.z * other.z;
+    }
+}
+
+impl ops::Add<Vector3> for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, _rhs: Vector3) -> Vector3 {
+        Vector3 { x: self.x + _rhs.x,
+            y: self.y + _rhs.y,
+            z: self.z + _rhs.z,
+        }
     }
 }
