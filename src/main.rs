05cba6f4b083280bbf0ac1b7a318b9a720eea53a
Richard Osborne <rosborne255@yahoo.com>
2025-08-17 21:00:00 -0500

    print mag of a

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ fn main() {
     let b: Vector3 = Vector3 { x: 0.0, y: 1.0, z: 0.0 };
     println!("dot prod a*b ({})", a.dot(&b));
     println!("cross prod a x b ({})", a.cross(&b));
-    println!("a magnitude ({})", a.mag());
+    println!("magnitude of vector a ({})", a.mag());
 }
