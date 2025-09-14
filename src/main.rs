d9d9b06b77005a51a181f8ab9621e227119787fb
Richard Osborne <rosborne255@yahoo.com>
2025-09-13 20:00:00 -0500

    rename vars into right/up

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ mod vector3;
 use vector3::Vector3;
 
 fn main() {
-    let a: Vector3 = Vector3 { x: 1.0, y: 0.0, z: 0.0 };
-    let b: Vector3 = Vector3 { x: 0.0, y: 1.0, z: 0.0 };
-    println!("dot prod a*b ({})", a.dot(&b));
-    println!("cross prod a x b ({})", a.cross(&b));
-    println!("magnitude of vector a ({})", a.mag());
+    let right: Vector3 = Vector3 { x: 1.0, y: 0.0, z: 0.0 };
+    let up: Vector3 = Vector3 { x: 0.0, y: 1.0, z: 0.0 };
+    println!("dot prod right*up ({})", right.dot(&up));
+    println!("cross prod right x up ({})", right.cross(&up));
+    println!("magnitude of vector right ({})", right.mag());
 }
