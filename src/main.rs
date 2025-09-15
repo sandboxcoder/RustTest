b31cc7550e1e9b7fd98c84c14304fb17020c2704
Richard Osborne <rosborne255@yahoo.com>
2025-09-14 20:00:00 -0500

    take cross product of right.cross(forward)

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,6 @@ fn main() {
     println!("dot prod right*up ({})", right.dot(&up));
     println!("cross prod right x up ({})", right.cross(&up));
     println!("magnitude of vector right ({})", right.mag());
+    let forward: Vector3 = Vector3 { x: 0.0, y: 0.0, z: 1.0 };
+    println!("cross prod right x forward ({})", right.cross(&forward));
 }
